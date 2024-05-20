6cdc644218ae1ccf6ab91a9dbc0e1f773613d5df
Mohamed Ashraf <urboifox@gmail.com>
2024-05-20 22:10:14 +0300

    feat: added session content load state

--- src/app/session/loading.tsx
+++ src/app/session/loading.tsx
@@ -1,7 +1,25 @@
+import Button from "@/components/common/Button";
+import SessionContentLoading from "@/components/skeletons/session/SessionContentLoading";
+import icons from "@/lib/icons";
+import Link from "next/link";
+
 export default function SessionPageLoading() {
   return (
-    <div className="container page flex items-center justify-center">
-    loading...
+    <div className='container page flex flex-col'>
+        <div className="flex gap-3 items-center">
+          <Link href={'/'}>
+            <Button>
+              {icons.angleLeft} Back
+            </Button>
+          </Link>
+          <Link href={'/session/list'}>
+            <Button className="text-2xl">
+              {icons.listUnordered}
+            </Button>
+          </Link>
+        </div>
+
+        <SessionContentLoading />
     </div>
   )
 }
---
+++ src/components/skeletons/session/SessionContentLoading.tsx
@@ -0,0 +1,41 @@
+import Button from '@/components/common/Button'
+import Input from '@/components/common/Input'
+import icons from '@/lib/icons'
+import React from 'react'
+
+export default function SessionContentLoading() {
+  return (
+          <div className="flex-1 pb-5 flex justify-between flex-col gap-10 items-center">
+              <div className="flex-1 flex-col gap-10 flex items-center justify-center">
+                <Input
+                    name="sessionName"
+                    placeholder="Session Name"
+                    className="transition-all duration-200"
+                />
+
+                      <div
+                          className="rounded-full opacity-50 cursor-not-allowed relative aspect-square w-40 flex items-center justify-center bg-white overflow-hidden border-white border text-4xl"
+                      >
+                        <span className='text-black'>
+                            {icons.play}
+                        </span>
+                      </div>
+
+                  <div className="flex items-center gap-5">
+                          <Button disabled >
+                              {icons.reset}
+                          </Button>
+                        <Button disabled >
+                            {icons.stop}
+                        </Button>
+                  </div>
+              </div>
+
+              <time
+                  className={"font-number select-none text-2xl font-light transition-colors duration-200"}
+              >
+                  {'00:00:00'}
+              </time>
+          </div>
+  )
+}
