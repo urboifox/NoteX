929c92b3b23492d653e6a33042c02fdda3a0ccef
Mohamed Ashraf <urboifox@gmail.com>
2024-05-25 03:06:50 +0300

    feat: working /ai api

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,43 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import Groq from "groq-sdk";
-
-const groq = new Groq({
-    apiKey: process.env.GROQ_API_KEY!,
-})
-
-export async function GET(req: NextRequest) {
-
-    const searchParams = req.nextUrl.searchParams;
-    const text = searchParams.get("text");
-
-    if (!text) {
-        return NextResponse.json({ data: null }, { status: 400 });
-    }
-
-    const completion = await groq.chat.completions.create({
-        messages: [
-            {
-                role: "system",
-                content: `You are an autocomplete assistant. 
-For the text content I provide as input, please give me a single text suggestion ranging from 2 to 8 words.
-If I send an uncomplete text, start your response with the completion of the text first then the suggestion.
-Start with a white space if needed.
-Start with a white space if my text content doesn't end with one.
-Start with a new line if needed.
-All the words should be complete.
-DO NOT give more than one suggestion.
-Do not add any names. Do Not add full stops in the end.`,
-            },
-            {
-                role: "user",
-                content: text!,
-            }
-        ],
-        model: "llama3-8b-8192",
-    });
-
-    return NextResponse.json({ data: completion.choices[0].message.content || "" }, { status: 200 });
-}
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export async function POST(req: NextRequest) {
 
@@ -47,27 +9,41 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ data: null }, { status: 400 });
     }
 
-    const completion = await groq.chat.completions.create({
-        messages: [
-            {
-                role: "system",
-                content: `You are an autocomplete assistant. 
-For the text content I provide as input, please give me a single text suggestion ranging from 2 to 8 words.
-If I send an uncomplete text, start your response with the completion of the text first then the suggestion.
-Start with a white space if needed.
-Start with a white space if the user text doesn't end with one.
-Start with a new line if needed.
-All the words should be complete.
-DO NOT give more than one suggestion.
-Do not add any names. Do Not add full stops in the end.`,
-            },
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+    const chat = model.startChat({
+        history: [
             {
                 role: "user",
-                content: text!,
+                parts: [
+                    { text: "You are an Autocomplete assistant." },
+                    { text: "For the text content I provide as input, please give me a single text completion suggestion ranging from 2 to 8 words." },
+                    { text: "If I send an uncomplete text, start your response with the completion of the text first then the suggestion." },
+                    { text: "Start with a white space if needed." },
+                    { text: "Start with a white space if my text content is uncomplete." },
+                    { text: "ONLY response with the suggestion, dont re send my message." },
+                    { text: "Make sure you dont re send my text. for example if i give you: hello there m. you will get me: om, how you been" },
+                    { text: "DO NOT give more than one suggestion." },
+                    { text: "All the words should be complete." },
+                    { text: "IF the last word of my text is complete, start your response with a white space if needed." },
+                    { text: "DO NOT give more than one suggestion." },
+                    { text: "DO NOT include new lines. in any part of the response, only text, and dont add forward slashes." },
+                    { text: "Do not add any names. Do Not add full stops in the end." },
+                ],
+            },
+            {
+                role: "model",
+                parts: [{ text: "Sure, please send your text" }],
             }
         ],
-        model: "llama3-8b-8192",
+        generationConfig: {
+            maxOutputTokens: 100,
+        },
     });
 
-    return NextResponse.json({ data: completion.choices[0].message.content || "" }, { status: 200 });
+    const result = await chat.sendMessage(text);
+    const response = result.response.text();
+
+    return NextResponse.json({ data: response }, { status: 200 });
 }
