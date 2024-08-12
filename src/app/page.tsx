0177b42689e5754a7bb24b335228b1ce592baa12
fox <urboifox@gmail.com>
2024-08-12 04:52:43 +0300

    add prefetch to home links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,65 +4,78 @@ import icons from "@/lib/icons";
 import Link from "next/link";
 
 const links: {
-    label: string,
-    href: string,
-    icon: React.ReactNode,
-    disabled?: boolean,
+    label: string;
+    href: string;
+    icon: React.ReactNode;
+    disabled?: boolean;
 }[] = [
-  {
-    label: "Blog",
-    href: "/blog",
-    icon: icons.pen,
-  },
-  {
-    label: "Todos",
-    href: "/todos",
-    icon: icons.todo,
-  },
-  {
-    label: "Session",
-    href: "/session",
-    icon: icons.clock,
-  },
-  {
-    label: "Board",
-    href: "/board",
-    icon: icons.paint,
-  },
-  // {
-  //   label: "Notes",
-  //   href: "/notes",
-  //   icon: icons.note,
-  //   disabled: true,
-  // },
-  // {
-  //   label: "Schedule",
-  //   href: "/schedule",
-  //   icon: icons.calendar,
-  //   disabled: true,
-  // },
+    {
+        label: "Blog",
+        href: "/blog",
+        icon: icons.pen,
+    },
+    {
+        label: "Todos",
+        href: "/todos",
+        icon: icons.todo,
+    },
+    {
+        label: "Session",
+        href: "/session",
+        icon: icons.clock,
+    },
+    {
+        label: "Board",
+        href: "/board",
+        icon: icons.paint,
+    },
+    // {
+    //   label: "Notes",
+    //   href: "/notes",
+    //   icon: icons.note,
+    //   disabled: true,
+    // },
+    // {
+    //   label: "Schedule",
+    //   href: "/schedule",
+    //   icon: icons.calendar,
+    //   disabled: true,
+    // },
 ];
 
 export default function Home() {
-  return (
-    <div className="container flex items-center justify-center h-[calc(100vh-5rem)]">
-      <div className="flex flex-col items-center gap-8 justify-center">
-        <h1 className="text-3xl sm:text-4xl text-center font-light select-none">What are you looking for?</h1>
+    return (
+        <div className="container flex items-center justify-center h-[calc(100vh-5rem)]">
+            <div className="flex flex-col items-center gap-8 justify-center">
+                <h1 className="text-3xl sm:text-4xl text-center font-light select-none">
+                    What are you looking for?
+                </h1>
 
-        <ul className="flex justify-center items-center flex-wrap gap-4">
-          {links.map((link) => (
-            <li key={link.label}>
-              <Tooltip keepVisible position="bottom" title={link.label}>
-                <Link className="text-4xl" href={link?.disabled ? '' : link.href}>
-                  <Button disabled={link?.disabled} className="p-5">
-                    {link.icon}
-                  </Button>
-                </Link>
-              </Tooltip>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+                <ul className="flex justify-center items-center flex-wrap gap-4">
+                    {links.map((link) => (
+                        <li key={link.label}>
+                            <Tooltip
+                                keepVisible
+                                position="bottom"
+                                title={link.label}
+                            >
+                                <Link
+                                    prefetch={true}
+                                    className="text-4xl"
+                                    href={link?.disabled ? "" : link.href}
+                                >
+                                    <Button
+                                        disabled={link?.disabled}
+                                        className="p-5"
+                                    >
+                                        {link.icon}
+                                    </Button>
+                                </Link>
+                            </Tooltip>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
